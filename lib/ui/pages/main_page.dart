cf27e357ae318b9cae7b6a39bba6664dacd786f3
AbdullahPrasetio <adeprastio1996@gmail.com>
2021-09-23 16:12:31 +0700

    update

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -42,73 +42,75 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 5,
-        child: SafeArea(
-          bottom: false,
-          child: Scaffold(
-            key: _scaffoldKey,
-            drawer: NavDrawer(),
-            appBar: AppBar(
-                leading: IconButton(
-                  icon: Icon(
-                    Icons.menu,
-                    color: Color(0XFF03203F),
-                  ),
-                  onPressed: () {
-                    _scaffoldKey.currentState.openDrawer();
-                  },
-                ),
-                backgroundColor: Colors.white,
-                title: Image.asset(
-                  "assets/images/Appbar.jpeg",
-                  width: 300,
-                  height: 250,
-                )),
-            body: Stack(
-              children: [
-                Container(
-                  color: Colors.white,
-                ),
-                SafeArea(
-                    child: Container(
-                  color: "FAFAFC".toColor(),
-                )),
-                SafeArea(
-                    child: PageView(
-                  controller: pageController,
-                  onPageChanged: (index) {
-                    setState(() {
-                      selectedPage = index;
-                    });
-                  },
+    return WillPopScope(
+        onWillPop: _onWillPop,
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: DefaultTabController(
+            length: 5,
+            child: SafeArea(
+              bottom: false,
+              child: Scaffold(
+                key: _scaffoldKey,
+                drawer: NavDrawer(),
+                appBar: AppBar(
+                    leading: IconButton(
+                      icon: Icon(
+                        Icons.menu,
+                        color: Color(0XFF03203F),
+                      ),
+                      onPressed: () {
+                        _scaffoldKey.currentState.openDrawer();
+                      },
+                    ),
+                    backgroundColor: Colors.white,
+                    title: Image.asset(
+                      "assets/images/Appbar.jpeg",
+                      width: 300,
+                      height: 250,
+                    )),
+                body: Stack(
                   children: [
-                    HomePage(),
-                    PlaylistEdu(),
-                    InputClaim(),
-                    ComplaintPage(),
-                    HomePageProduct(),
+                    Container(
+                      color: Colors.white,
+                    ),
+                    SafeArea(
+                        child: Container(
+                      color: "FAFAFC".toColor(),
+                    )),
+                    SafeArea(
+                        child: PageView(
+                      controller: pageController,
+                      onPageChanged: (index) {
+                        setState(() {
+                          selectedPage = index;
+                        });
+                      },
+                      children: [
+                        HomePage(),
+                        PlaylistEdu(),
+                        InputClaim(),
+                        ComplaintPage(),
+                        HomePageProduct(),
+                      ],
+                    )),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: CustomBottomNavBar(
+                        selectedIndex: selectedPage,
+                        onTap: (index) {
+                          setState(() {
+                            selectedPage = index;
+                          });
+                          pageController.jumpToPage(selectedPage);
+                        },
+                      ),
+                    )
                   ],
-                )),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: CustomBottomNavBar(
-                    selectedIndex: selectedPage,
-                    onTap: (index) {
-                      setState(() {
-                        selectedPage = index;
-                      });
-                      pageController.jumpToPage(selectedPage);
-                    },
-                  ),
-                )
-              ],
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
